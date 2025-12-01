76d11b842b5cb2766b7eaa53f5e9e84bfd67fc91
Okky <okky.mstafford@gmail.com>
2025-12-01 11:43:55 +1100

    added config queue in application config file

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module Vantura
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # Use Sidekiq for background jobs
+    config.active_job.queue_adapter = :sidekiq
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
